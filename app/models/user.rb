ba02da452698b0fe684d4efd67161f2772ee9a28
acordova <ae_cordova@outlook.com>
2020-03-09 17:42:19 -0600

    Added digest, new token and create remember token methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,23 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
 
-    has_secure_password
+  before_create :create_remember_token
+
+  has_secure_password
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.digest(string)
+    Digest::SHA1.hexdigest(string)
+  end
+
+
+  
+  def create_remember_token
+    self.remember_token = User.digest(User.new_token)
+  end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_09_213318) do
+ActiveRecord::Schema.define(version: 2020_03_09_222512) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_03_09_213318) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "remember_token"
   end
 
 end
