b87eb45baf6a6a3c95508cf6cac9a7497723e5de
Rey David Cuevas <cuevas.vela@gmail.com>
2020-03-11 14:09:41 -0600

    added user#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      redirect_to @user
+    else
+      flash.now[:danger] = 'Invalid user please try again'
+      render :new
+    end
+  end
 
+  private
+  def user_params
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
   end
 end
