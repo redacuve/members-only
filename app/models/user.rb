16c7f5f503d79725ae34653cfb0a76983fc437aa
acordova <ae_cordova@outlook.com>
2020-03-11 11:19:17 -0600

    Added simple index view for posts and created associations for posts and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   before_create :remember
   has_secure_password
 
+  has_many :post
+
   def remember
     self.remember_token = Digest::SHA1.hexdigest(SecureRandom.urlsafe_base64)
   end
