9a579a54b36c34799a94a733ceab84dee49c2b84
Rey David Cuevas <cuevas.vela@gmail.com>
2020-03-11 10:33:16 -0600

    added some stylish to the post index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,22 @@
 class PostsController < ApplicationController
   before_action :allow_posts, only: [:new, :create]
 
-  def new; end
+  def new
+    @post = Post.new
+  end
+
+
+  def index
+    @posts = Post.all
+  end
 
   def create
-
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render html: "intenta de nuevo"
+    end
   end
 
   def allow_posts
@@ -12,6 +24,9 @@ class PostsController < ApplicationController
     redirect_to new_session_path unless logged?
   end
 
+  private
 
-
+  def post_params
+    params.require(:post).permit(:title, :body, :author_id)
+  end
 end
